372ea88b38b514243929fdaa119df1bae4ccce8e
Francesco Bedussi <francesco@francescobedussi.it>
2019-02-21 22:07:58 +0100

    client side issue filtering

--- src/Issue/IssueList/index.js
+++ src/Issue/IssueList/index.js
@@ -6,6 +6,26 @@ import ErrorMessage from '../../Error';
 import Loading from '../../Loading';
 import IssueItem from '../IssueItem';
 
+const ISSUE_STATE = {
+    NONE: 'NONE',
+    OPEN: 'OPEN',
+    CLOSED: 'CLOSED',
+}
+
+const isShown = (issueState) => issueState !== ISSUE_STATE.NONE;
+
+const TRANSITION_LABELS = {
+    [ISSUE_STATE.NONE]: 'Show open issues',
+    [ISSUE_STATE.OPEN]: 'Show closed issues',
+    [ISSUE_STATE.CLOSED]: 'Hide issues',
+};
+
+const TRANSITION_STATE = {
+    [ISSUE_STATE.NONE]: ISSUE_STATE.OPEN,
+    [ISSUE_STATE.OPEN]: ISSUE_STATE.CLOSED,
+    [ISSUE_STATE.CLOSED]: ISSUE_STATE.NONE,
+};
+
 const GET_ISSUES_OF_REPOSITORY = gql`
     query($repositoryName: String!, $repositoryOwner: String!) {
         repository (name: $repositoryName, owner: $repositoryOwner) {
@@ -25,31 +45,57 @@ const GET_ISSUES_OF_REPOSITORY = gql`
     }
 `;
 
-const Issues = ({ repositoryName, repositoryOwner }) =>
-    <Query
-        query={GET_ISSUES_OF_REPOSITORY}
-        variables={{
-            repositoryName,
-            repositoryOwner,
-        }}
-    >
-        {({ data, loading, error }) => {
-            if (error) {
-                return <ErrorMessage error={error} />
-            }
+class Issues extends React.Component {
+    state = {
+        issueState: ISSUE_STATE.NONE,
+    }
 
-            const { repository } = data;
-            
-            if (loading && !repository) {
-                return <Loading />
-            }
+    onChangeIssueState = (issueState) => this.setState({issueState});
 
-            if (!repository.issues.edges.length) {
-                return <div>No issues found...</div>
-            }
+    render() {
+        const {issueState} = this.state;
+        const { repositoryName, repositoryOwner } = this.props;
 
-            return repository.issues.edges.map((issue) => <IssueItem key={issue.node.id} issue={issue.node} />)
-        }}
-    </Query>;
+        return <div className="issues">
+            <button
+                onClick={() => this.onChangeIssueState(TRANSITION_STATE[issueState])}
+            >
+                {TRANSITION_LABELS[issueState]}
+            </button>
+            {isShown(issueState) && <Query
+                query={GET_ISSUES_OF_REPOSITORY}
+                variables={{
+                    repositoryName,
+                    repositoryOwner,
+                }}
+            >
+                {({ data, loading, error }) => {
+                    if (error) {
+                        return <ErrorMessage error={error} />
+                    }
+
+                    const { repository } = data;
+
+                    if (loading && !repository) {
+                        return <Loading />
+                    }
+
+                    const filteredRepository = {
+                        issues: {
+                            edges: repository.issues.edges.filter((issue) => issue.node.state === issueState)
+                        }
+                    }
+
+                    if (!filteredRepository.issues.edges.length) {
+                        return <div>No issues found...</div>
+                    }
+
+                    return filteredRepository.issues.edges.map((issue) => <IssueItem key={issue.node.id} issue={issue.node} />)
+                }}
+            </Query>}
+        </div>;
+    }
+}
+
 
 export default Issues;
